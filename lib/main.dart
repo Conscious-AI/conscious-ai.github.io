2a554e7a2a3985c10821f5a80e82c84ab14675b3
Abhishek Dubey <abhi.dubey011999@gmail.com>
2023-01-30 05:28:51 +0530

    some magic
    Signed-off-by: Abhishek Dubey <abhi.dubey011999@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
 import 'package:motion/motion.dart';
+import 'package:video_player/video_player.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+const caiTextList = [
+  'Conscius AI', // Latin
+  'जागरूक एआई', // Hindi
+  'IA consciente', // French
+  'Bewusste KI', // German
+  'Сознательный ИИ', // Russian
+  '有意识的人工智能', // Chinese
+  'Συνειδητή AI', // Greek
+  '意識的な AI', // Japanese
+  'AI מודע', //  Hebrew
+  '의식 있는 AI', // Korean
+];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,12 +34,9 @@ class CAI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'ConsciusAI',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Landing(),
+      home: Landing(),
     );
   }
 }
@@ -37,25 +49,63 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network('https://github.com/Conscious-AI/conscious-ai.github.io/raw/main/assets/videos/lights.mp4')
+      ..initialize().then((_) {
+        setState(() {
+          _controller
+            ..setLooping(true)
+            ..play();
+        });
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        child: Stack(
-          children: [
-            Motion.elevated(
-              elevation: 50,
-              child: Center(
-                child: Text(
-                  'ConsciusAI',
-                  style: TextStyle(
-                    fontFamily: 'Eurostile',
-                    color: Colors.black,
-                  ),
+        child: Motion.elevated(
+          elevation: 150,
+          glare: false,
+          child: Stack(
+            children: [
+              _controller.value.isInitialized
+                  ? SizedBox.expand(
+                      child: FittedBox(
+                        fit: BoxFit.cover,
+                        child: SizedBox(
+                          width: _controller.value.size.width,
+                          height: _controller.value.size.height,
+                          child: VideoPlayer(_controller),
+                        ),
+                      ),
+                    )
+                  : Container(),
+              Center(
+                child: AnimatedTextKit(
+                  repeatForever: true,
+                  totalRepeatCount: caiTextList.length,
+                  animatedTexts: [
+                    for (int i = 0; i < caiTextList.length; i++)
+                      FlickerAnimatedText(
+                        caiTextList[i],
+                        textStyle: const TextStyle(
+                          fontFamily: 'Eurostile',
+                          fontSize: 72,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(seconds: 3),
+                      ),
+                  ],
                 ),
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
